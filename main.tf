9ae5a042923207b13313911ea7e7aa863fd58b12
Daniel Ananth <lca_daniel_a@yahoo.com>
2018-06-25 21:12:32 -0400

    26 final main.tf update

--- main.tf
+++ main.tf
@@ -5,29 +5,60 @@ variable "instance_ips" {
   }
 }
 
+variable "public_key_path" {
+  description = "Enter the path to the SSH Public Key to add to AWS."
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = "default"
+  public_key = "${file(var.public_key_path)}"
+}
+
 resource "aws_instance" "fabric" {
   count                  = 2
   private_ip             = "${lookup(var.instance_ips, count.index)}"
   ami                    = "ami-68b8e517"
   instance_type          = "t2.micro"
+  key_name               = "default"
   subnet_id              = "${aws_subnet.fabric-subnet.id}"
   vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
 }
 
 resource "aws_vpc" "fabric-vpc" {
-  cidr_block = "192.168.1.0/16"
+  cidr_block           = "192.168.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.fabric-vpc.id}"
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.fabric-vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
 }
 
 resource "aws_subnet" "fabric-subnet" {
-  vpc_id            = "${aws_vpc.fabric-vpc.id}"
-  cidr_block        = "192.168.1.1/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = "${aws_vpc.fabric-vpc.id}"
+  cidr_block              = "192.168.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 }
 
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
   description = "Allow HTTP traffic"
   vpc_id      = "${aws_vpc.fabric-vpc.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 8080
