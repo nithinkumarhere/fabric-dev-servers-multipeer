c6e0906d49502c279293530bd6386611bb4cfe07
Nithin Kumar K <nithinkumarhere@gmail.com>
2019-02-01 11:07:22 +0530

    Fabric with 3-Orgs on 3Nodes

--- composer/howtobuild.sh
+++ composer/howtobuild.sh
@@ -2,10 +2,13 @@ cd "$(dirname "$0")"
 
 HOST1="192.168.1.222"
 HOST2="192.168.1.224"
+HOST3="192.168.1.224"
 
 sed -i -e "s/{IP-HOST-1}/$HOST1/g" configtx.yaml
 sed -i -e "s/{IP-HOST-1}/$HOST1/g" ../startFabric-Peer2.sh
+sed -i -e "s/{IP-HOST-1}/$HOST1/g" ../startFabric-Peer3.sh
 sed -i -e "s/{IP-HOST-2}/$HOST2/g" ../createPeerAdminCard.sh
+sed -i -e "s/{IP-HOST-3}/$HOST3/g" ../createPeerAdminCard.sh
 
 cryptogen generate --config=./crypto-config.yaml
 export FABRIC_CFG_PATH=$PWD
@@ -14,7 +17,9 @@ configtxgen -profile ComposerChannel -outputCreateChannelTx ./composer-channel.t
 
 ORG1KEY="$(ls crypto-config/peerOrganizations/org1.example.com/ca/ | grep 'sk$')"
 ORG2KEY="$(ls crypto-config/peerOrganizations/org2.example.com/ca/ | grep 'sk$')"
+ORG3KEY="$(ls crypto-config/peerOrganizations/org3.example.com/ca/ | grep 'sk$')"
 
 sed -i -e "s/{ORG1-CA-KEY}/$ORG1KEY/g" docker-compose.yml
 sed -i -e "s/{ORG2-CA-KEY}/$ORG2KEY/g" docker-compose-peer2.yml
+sed -i -e "s/{ORG3-CA-KEY}/$ORG3KEY/g" docker-compose-peer3.yml
 
--- createPeerAdminCard.sh
+++ createPeerAdminCard.sh
@@ -8,6 +8,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Grab the file names of the keystore keys
 ORG1KEY="$(ls composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/)"
 ORG2KEY="$(ls composer/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/keystore/)"
+ORG3KEY="$(ls composer/crypto-config/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/keystore/)"
 
 echo
 # check that the composer command exists at a version >v0.14
@@ -227,15 +228,133 @@ composer card import --file /tmp/PeerAdmin@byfn-network-org2.card
 
 rm -rf org2onlyconnection.json
 
+cat << EOF > org3onlyconnection.json
+{
+    "name": "byfn-network-org3-only",
+    "type": "hlfv1",
+    "orderers": [
+        {
+            "url" : "grpc://localhost:7050",
+            "hostnameOverride": "orderer.example.com"
+        }
+    ],
+    "ca": {
+        "url": "http://{IP-HOST-3}:7054",
+        "name": "ca.org3.example.com",
+        "hostnameOverride": "ca.org3.example.com"
+    },
+    "peers": [
+        {
+            "requestURL": "grpc://{IP-HOST-3}:10051",
+            "eventURL": "grpc://{IP-HOST-3}:10053",
+            "hostnameOverride": "peer0.org3.example.com"
+        }, {
+            "requestURL": "grpc://{IP-HOST-3}:11051",
+            "eventURL": "grpc://{IP-HOST-3}:11053",
+            "hostnameOverride": "peer1.org3.example.com"
+
+        }, {
+            "requestURL": "grpc://{IP-HOST-3}:12051",
+            "eventURL": "grpc://{IP-HOST-3}:12053",
+            "hostnameOverride": "peer2.org3.example.com"
+        }
+    ],
+    "channel": "composerchannel",
+    "mspID": "Org3MSP",
+    "timeout": 300
+}
+EOF
+
+cat << EOF > org3connection.json
+{
+    "name": "byfn-network-org2",
+    "type": "hlfv1",
+    "orderers": [
+        {
+            "url" : "grpc://localhost:7050",
+            "hostnameOverride": "orderer.example.com"
+        }
+    ],
+    "ca": {
+        "url": "http://{IP-HOST-2}:7054",
+        "name": "ca.org2.example.com",
+        "hostnameOverride": "ca.org2.example.com"
+    },
+    "peers": [
+        {
+            "requestURL": "grpc://localhost:7051",
+            "hostnameOverride": "peer0.org1.example.com"
+        }, {
+            "requestURL": "grpc://localhost:8051",
+            "hostnameOverride": "peer1.org1.example.com"
+        }, {
+            "requestURL": "grpc://localhost:9051",
+            "hostnameOverride": "peer2.org1.example.com"
+        }, {
+            "requestURL": "grpc://{IP-HOST-2}:10051",
+            "eventURL": "grpc://{IP-HOST-2}:10053",
+            "hostnameOverride": "peer0.org2.example.com"
+        }, {
+            "requestURL": "grpc://{IP-HOST-2}:11051",
+            "eventURL": "grpc://{IP-HOST-2}:11053",
+            "hostnameOverride": "peer1.org2.example.com"
+        }, {
+            "requestURL": "grpc://{IP-HOST-2}:12051",
+            "eventURL": "grpc://{IP-HOST-2}:12053",
+            "hostnameOverride": "peer2.org2.example.com"
+        }, {
+            "requestURL": "grpc://{IP-HOST-3}:10051",
+            "eventURL": "grpc://{IP-HOST-3}:10053",
+            "hostnameOverride": "peer0.org3.example.com"
+        }, {
+            "requestURL": "grpc://{IP-HOST-3}:11051",
+            "eventURL": "grpc://{IP-HOST-3}:11053",
+            "hostnameOverride": "peer1.org3.example.com"
+        }, {
+            "requestURL": "grpc://{IP-HOST-3}:12051",
+            "eventURL": "grpc://{IP-HOST-3}:12053",
+            "hostnameOverride": "peer2.org3.example.com"
+        }
+    ],
+    "channel": "composerchannel",
+    "mspID": "Org3MSP",
+    "timeout": 300
+}
+EOF
+
+PRIVATE_KEY="${DIR}"/composer/crypto-config/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/keystore/"${ORG3KEY}"
+CERT="${DIR}"/composer/crypto-config/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/signcerts/Admin@org3.example.com-cert.pem
+
+
+if composer card list -n @byfn-network-org3-only > /dev/null; then
+    composer card delete -n @byfn-network-org3-only
+fi
+
+if composer card list -n @byfn-network-org3 > /dev/null; then
+    composer card delete -n @byfn-network-org3
+fi
+
+composer card create -p org3onlyconnection.json -u PeerAdmin -c "${CERT}" -k "${PRIVATE_KEY}" -r PeerAdmin -r ChannelAdmin --file /tmp/PeerAdmin@byfn-network-org3-only.card
+composer card import --file /tmp/PeerAdmin@byfn-network-org3-only.card
+
+composer card create -p org3connection.json -u PeerAdmin -c "${CERT}" -k "${PRIVATE_KEY}" -r PeerAdmin -r ChannelAdmin --file /tmp/PeerAdmin@byfn-network-org3.card
+composer card import --file /tmp/PeerAdmin@byfn-network-org3.card
+
+rm -rf org3onlyconnection.json
+
 echo "Hyperledger Composer PeerAdmin card has been imported"
 composer card list
 
-composer runtime install -c PeerAdmin@byfn-network-org1-only -n trade-network
-composer runtime install -c PeerAdmin@byfn-network-org2-only -n trade-network
-composer identity request -c PeerAdmin@byfn-network-org1-only -u admin -s adminpw -d alice
-composer identity request -c PeerAdmin@byfn-network-org2-only -u admin -s adminpw -d bob
-composer network start -c PeerAdmin@byfn-network-org1 -a trade-network.bna -o endorsementPolicyFile=endorsement-policy.json -A alice -C alice/admin-pub.pem -A bob -C bob/admin-pub.pem
-composer card create -p org1connection.json -u alice -n trade-network -c alice/admin-pub.pem -k alice/admin-priv.pem
-composer card import -f alice@trade-network.card
-composer card create -p org2connection.json -u bob -n trade-network -c bob/admin-pub.pem -k bob/admin-priv.pem
-composer card import -f bob@trade-network.card
+composer runtime install -c PeerAdmin@byfn-network-org1-only -n KYC-network
+composer runtime install -c PeerAdmin@byfn-network-org2-only -n KYC-network
+composer runtime install -c PeerAdmin@byfn-network-org3-only -n KYC-network
+composer identity request -c PeerAdmin@byfn-network-org1-only -u admin -s adminpw -d CentralBank
+composer identity request -c PeerAdmin@byfn-network-org2-only -u admin -s adminpw -d BankA
+composer identity request -c PeerAdmin@byfn-network-org3-only -u admin -s adminpw -d BankB
+composer network start -c PeerAdmin@byfn-network-org1 -a KYC-network.bna -o endorsementPolicyFile=endorsement-policy.json -A CentralBank -C CentralBank/admin-pub.pem -A BankA -C BankA/admin-pub.pem -A BankB -C BankB/admin-pub.pem
+composer card create -p org1connection.json -u CentralBank -n KYC-network -c CentralBank/admin-pub.pem -k CentralBank/admin-priv.pem
+composer card import -f CentralBank@KYC-network.card
+composer card create -p org2connection.json -u BankA -n KYC-network -c BankA/admin-pub.pem -k BankA/admin-priv.pem
+composer card import -f BankA@KYC-network.card
+composer card create -p org3connection.json -u BankB -n KYC-network -c BankB/admin-pub.pem -k BankB/admin-priv.pem
+composer card import -f BankB@KYC-network.card
